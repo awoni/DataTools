02188136f3853fcb6282790402bdd0d834e76dfb
Yasuhiro Niji <awoni@awoni.net>
2016-02-16 13:46:56 +0900

    NlogがUbuntuではうまく動かないようなので

--- DataReadSaveTime/Models/LoggerClass.cs
+++ DataReadSaveTime/Models/LoggerClass.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NLog;
+using System.IO;
 
 namespace DataReadSaveTime.Models
 {
     class LoggerClass
     {
+        //NlogがUbuntuではうまく動かないようなので
+        /*
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public static void NLogInfo(string message)
         {
             logger.Info(message);
-        }        
+        } 
+        */
+        public static void NLogInfo(string message)
+        {
+            using(var fs = new FileStream("data/log-" + DateTime.Now.ToString("yyyy-MM-dd") + ".log", FileMode.Append, FileAccess.Write))
+            using (var sr = new StreamWriter(fs))
+            {
+                string s = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {message}";
+                sr.Write(s);
+            }
+        }
+
+
     }
 }
